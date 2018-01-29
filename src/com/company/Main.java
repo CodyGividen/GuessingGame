33c8e5aeddb0bfcdc330b506bb988d0f1802e098
CodyGividen <codygividen5@gmail.com>
2018-01-29 15:01:51 -0500

    Added a Do while inside do while

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,29 +14,31 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         int numberOfGuess = (0);
-
+        int userGuess = input.nextInt();
         final int MAX_GUESS_COUNT = 4;
 
-        System.out.println("Enter Your Guess:");
-        int userGuess = input.nextInt();
-
         int randonumber = (int) Math.ceil(Math.random() * 100);
-
+        do {
             do {
                 System.out.println("Enter Your Guess:");
-                int userGuess = input.nextInt();
-                
                 if (userGuess > randonumber) {
                     System.out.println("your number is to high");
                     numberOfGuess = (numberOfGuess + 1);
                 } else if (userGuess < randonumber) {
                     System.out.println("Your number is too low!");
                     numberOfGuess = (numberOfGuess + 1);
-                }else if (userGuess == randonumber) {
+                } else if (userGuess == randonumber) {
                     System.out.println("You guessed the correct number!!");
-                }else if (numberOfGuess == MAX_GUESS_COUNT){
-                    System.out.println( "Sorry, you have ran out of chances. The correct number was:" + randonumber);
+                } else if (numberOfGuess >= MAX_GUESS_COUNT) {
+                    System.out.println("Sorry, you have ran out of chances. The correct number was:" + randonumber);
                 }
-            } while (numberOfGuess <= 4);
-        }
+            } while (numberOfGuess == 5);
+            System.out.println("Would you like to play again? 200 for Yes, 300 for No");
+                if (userGuess == 300) {
+                    System.exit(0);
+                }else if(userGuess == 200) {
+                    numberOfGuess = (numberOfGuess - 4);
+                    }
+        }while (numberOfGuess <= MAX_GUESS_COUNT) ;
     }
+}
